e272e8c31be14089a0e5791ee9c19f070a65d91b
mrpiggy97 <fabyjesusrivas10@gmail.com>
2021-12-09 23:38:50 +0000

    made the basic grpc server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+
+	"github.com/mrpiggy97/grpc-example/user"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("hello grpc")
+	listener, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		panic(err)
+	}
+	var grpcServer *grpc.Server = grpc.NewServer()
+	var userServerInstance *user.UserServer = new(user.UserServer)
+	user.RegisterUserServiceServer(grpcServer, userServerInstance)
+	fmt.Println("server is listening at port 50051")
+	if listeningErr := grpcServer.Serve(listener); listeningErr != nil {
+		panic(listeningErr)
+	}
 }

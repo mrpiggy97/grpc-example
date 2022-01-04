96aeb365c65642082f85248cf72b356f51464851
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-01-04 21:21:25 +0000

    created servers to be consumed,and updated package

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
-module github.com/mrpiggy97/grpc-example
+module github.com/mrpiggy97/grpcExample
 
 go 1.16
 
 require (
+	github.com/mrpiggy97/sharedProtofiles v1.0.1
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
 	google.golang.org/grpc v1.42.0
-	google.golang.org/protobuf v1.27.1
 )
--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"net"
 
-	"github.com/mrpiggy97/grpc-example/user"
+	"github.com/mrpiggy97/sharedProtofiles/calculation"
+	"github.com/mrpiggy97/sharedProtofiles/formatting"
+	"github.com/mrpiggy97/sharedProtofiles/num"
+	"github.com/mrpiggy97/sharedProtofiles/randomNumber"
+	"github.com/mrpiggy97/sharedProtofiles/user"
+
 	"google.golang.org/grpc"
 )
 
@@ -13,9 +18,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	var userServer *user.Server = new(user.Server)
 	var grpcServer *grpc.Server = grpc.NewServer()
-	var userServerInstance *user.UserServer = new(user.UserServer)
-	user.RegisterUserServiceServer(grpcServer, userServerInstance)
+	user.RegisterUserServiceServer(grpcServer, userServer)
+
+	var numServer *num.Server = new(num.Server)
+	num.RegisterNumServiceServer(grpcServer, numServer)
+
+	var formattingServer *formatting.Server = new(formatting.Server)
+	formatting.RegisterFormattingServiceServer(grpcServer, formattingServer)
+
+	var randomNumberServer *randomNumber.Server = new(randomNumber.Server)
+	randomNumber.RegisterRandomServiceServer(grpcServer, randomNumberServer)
+
+	var calculationServer *calculation.Server = new(calculation.Server)
+	calculation.RegisterCalculationServiceServer(grpcServer, calculationServer)
+
 	fmt.Println("server is listening at port 50051")
 	if listeningErr := grpcServer.Serve(listener); listeningErr != nil {
 		panic(listeningErr)
